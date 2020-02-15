499894b8baa9f38717ef456b927a4d2c670fd949
Adolfo Rodriguez <adolfo@mokriya.com>
2020-02-14 19:36:32 -0500

    experimenting with img params

--- based_latex/formula.py
+++ based_latex/formula.py
@@ -165,7 +165,7 @@ class Formula:
 		class_property = '' if class_name is None else ' class="' + class_name + '"'
 		static_style = ['position:relative;display:inline-block;pointer-events:none;', 'position:absolute;left:0;top:0;width:100%;height:200%;']
 		static_style = [(style if include_static_style else '') for style in static_style]
-		return f'<span{class_property} style="{static_style[0]}width:{em_height_max}em;height:{em_height_max}em;margin:{-em_width_delta}em {-em_top_delta}em {-em_width_delta}em {-em_bottom_delta}em"><img style="{static_style[1]}" src="',\
+		return f'<span{class_property} style="{static_style[0]}width:{em_height_max}em;height:{em_height_max}em;margin:{-em_top_delta}em {-em_width_delta}em {-em_bottom_delta}em {-em_width_delta}em;"><img style="{static_style[1]}" src="',\
 		       '"></span>'
 
 def save_latex_image(expression, path, density = 512, factor = 1, class_name = "latex", include_static_style = True, process_timeout = 2):
